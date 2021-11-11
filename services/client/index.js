d2e025f1d43cead203ec916e4d8036d1f19cb256
Nikita Luparev <nikita.luparev@gmail.com>
2021-11-11 19:42:06 +0500

    configure project

---
+++ services/client/index.js
@@ -0,0 +1,5 @@
+function main() {
+  console.log("Ohhh my gosshhh");
+}
+
+module.exports = main;
---
+++ services/server/index.js
@@ -0,0 +1,7 @@
+const client = require("@monorepo_app/client");
+
+function main() {
+  client();
+}
+
+main();
