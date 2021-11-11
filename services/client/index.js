e6d01147af8a855cf6e22a3fec3c0af8e052d986
Nikita Luparev <nikita.luparev@gmail.com>
2021-11-11 19:52:11 +0500

    add utils

--- services/client/index.js
+++
@@ -1,5 +0,0 @@
-function main() {
-  console.log("This is message from client!");
-}
-
-module.exports = main;
---
+++ services/utils/index.js
@@ -0,0 +1,5 @@
+function main() {
+  console.log("This message is from utils!");
+}
+
+module.exports = main;
